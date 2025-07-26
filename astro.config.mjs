738d6106e02677126aa6a5526c40c5cdb83e1c89
JonathanVegafz <jonathan.vega.f@mail.pucv.cl>
2025-07-26 15:45:10 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  site: 'https://JonathanVegafz.github.io',
+  base: 'landig_Arqui'
 });
