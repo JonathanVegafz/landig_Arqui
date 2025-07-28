845d3af2abd503ac1bffb6e8fc693ff38c30795f
Javier <javier.retamal.f@mail.pucv.cl>
2025-07-28 13:18:07 -0400

    Mejoras rendimiento

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,22 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://JonathanVegafz.github.io',
-  base: 'landig_Arqui'
+  base: 'landig_Arqui',
+  build: {
+    inlineStylesheets: 'auto'
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'vendor': ['aos']
+          }
+        }
+      }
+    },
+    css: {
+      devSourcemap: false
+    }
+  }
 });
